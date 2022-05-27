d7ac2206bed28f3d2109802a6169acd33d21b220
Kirill Leyfer <k.leyfer@drweb.com>
2022-05-27 21:27:06 +0300

    Fix call depth
    Signed-off-by: Kirill Leyfer <k.leyfer@drweb.com>

--- preprocessor.py
+++ preprocessor.py
@@ -68,21 +68,160 @@ def update_line_nums(asm_lines):
         line.num = i
 
 
-def main():
-    if len(sys.argv) != 2:
-        print("Usage: preprocessor.py <asm file>")
-        return
-
-    filename = sys.argv[1]
-    with open(filename, "r") as f:
-        text_lines = f.readlines()
-
+def process_asm(text_lines):
     asm_lines = []
     for i, line in enumerate(text_lines):
         asm_lines.append(AsmLine(line, i))
 
     # for line in asm_lines:
     #     print(line)
+
+    # handle call/ret
+    # Reserve one memory location for accumulator value
+    tmp_found = False
+    ret_found = False
+    for line in asm_lines:
+        if line.is_define:
+            if line.name == "TMP":
+                tmp_found = True
+            elif line.name == "RETURN_ADDRESS":
+                ret_found = True
+
+            if ret_found and tmp_found:
+                break
+
+    if not tmp_found:
+        print("No TMP variable!")
+    if not ret_found:
+        print("No RETURN_ADDRESS variable!")
+
+
+    calls = {}
+    for i, line in enumerate(asm_lines):
+        if line.is_instruction:
+            if line.opcode == "call" or line.opcode == "calli":
+                label = line.params[0]
+                if calls.get(label) is None:
+                    # [called, total, call_depth]
+                    calls[label] = [0, 0, 0]
+                else:
+                    calls[label][1] = calls[label][1] + 1
+
+    call_depth = 0
+    current_call = ""
+    for i, line in enumerate(asm_lines):
+        if line.is_label:
+            if calls.get(line.value) is not None:
+                calls.get(line.value).append(line.num)
+                current_call = line.value
+        elif line.is_instruction:
+            if line.opcode == "ret":
+                calls.get(current_call).append(line.num)
+
+    for call in calls:
+        line_start, line_end = calls[call][3], calls[call][4]
+        for i in range(line_start, line_end):
+            line = asm_lines[i]
+            if line.is_instruction:
+                if line.opcode in ("call", "calli"):
+                    if calls[line.params[0]][2] <= calls[call][2]:
+                        print("{} -> {}".format(call, line.params[0]))
+                        calls[line.params[0]][2] = calls[call][2] + 1
+
+    for call in calls:
+        print(call, calls[call])
+
+    call_found = True
+    while call_found:
+        call_found = False
+        call_index = 0
+        label = ""
+        insert_1 = []
+        insert_2 = []
+        return_address = "RETURN_ADDRESS"
+
+        for i, line in enumerate(asm_lines):
+            if line.is_instruction:
+                # calli = Call with parameter in accumulator
+                # call = no parameter
+                # So we save parameter for calli in TMP var
+                # TODO: remove TMP var, save RETURN_ADDRESS before lit/ld to accumulator
+                if line.opcode == "call" or line.opcode == "calli":
+                    call_found = True
+                    call_index = i
+                    label = line.params[0]
+                    count = calls[label][0]
+                    depth = calls[label][2]
+                    if depth != 0:
+                        return_address = "RETURN_ADDRESS_" + str(depth)
+                    calls[label][0] = calls[label][0] + 1
+                    if line.opcode == "calli":
+                        insert_1.append(AsmLine("{0:4}st TMP".format(line.local_label), 0))
+                        insert_1.append(AsmLine("    lit #" + str(count), 0))
+                    else:
+                        insert_1.append(AsmLine("{0:4}lit #{1}".format(line.local_label, count), 0))
+
+                    insert_1.append(AsmLine("    st " + return_address, 0))
+                    if line.opcode == "calli":
+                        insert_1.append(AsmLine("    ld TMP", 0))
+
+                    insert_1.append(AsmLine("    jmp " + label, 0))
+                    insert_1.append(AsmLine(label + "_ret_" + str(count) + ":", 0))
+
+                    if count == 0:
+                        insert_2.append(AsmLine("    ld " + return_address, 0))
+                    else:
+                        insert_2.append(AsmLine("    cmpi #" + str(count), 0))
+
+                    insert_2.append(AsmLine("    jz " + label + "_ret_" + str(count), 0))
+                    break
+
+        if call_found:
+            # print("Processing", line)
+            search_for_ret = False
+            return_index = 0
+
+            for i, line in enumerate(asm_lines):
+                if search_for_ret:
+                    if line.is_instruction:
+                        if line.opcode == "ret":
+                            return_index = i
+                            if line.is_local_label:
+                                if calls[label][0] == 1:
+                                    insert_2[0] = AsmLine("{0:4}ld {1}".format(line.local_label, return_address), 0)
+
+                            break
+                elif line.is_label:
+                    if line.value == asm_lines[call_index].params[0]:
+                        search_for_ret = True
+
+            if return_index == 0:
+                print("No return index for {0}!!!".format(asm_lines[call_index]))
+                return asm_lines
+
+            # print("  ", asm_lines[return_index-1].line)
+
+            if calls[label][0] > calls[label][1]:
+                # print("- ", asm_lines[return_index].line)
+                asm_lines.remove(asm_lines[return_index])
+                insert_2.append(AsmLine("    jmp halt ; invalid return address", 0))
+
+            asm_lines[return_index:return_index] = insert_2
+            # for insert in insert_2:
+                # print("+ ", insert.line)
+
+            # print("  ", asm_lines[return_index+len(insert_2) + 1].line)
+            # print("...")
+            # print("  ", asm_lines[call_index-1].line)
+            # print("- ", asm_lines[call_index].line)
+
+            asm_lines.remove(asm_lines[call_index])
+            asm_lines[call_index:call_index] = insert_1
+            # for insert in insert_1:
+                # print("+ ", insert.line)
+            # print("  ", asm_lines[call_index+len(insert_1) + 1].line)
+
+            update_line_nums(asm_lines)
 
     # handle sti
     sti_found = True
@@ -176,7 +315,7 @@ def main():
                     buf_idx = len(params) * 2 - 1 + buf_offset
                     if (buf_idx > 15):
                         print("Buffer is TOO LONG!!!")
-                        break
+                        return asm_lines
 
                     for j, param in enumerate(params):
                         prefix = ""
@@ -195,134 +334,24 @@ def main():
             asm_lines.remove(asm_lines[writebuf_index])
             asm_lines[writebuf_index:writebuf_index] = writebuf_insert
             update_line_nums(asm_lines)
+    return asm_lines
 
-    # handle call/ret
-    # Reserve one memory location for accumulator value
-    tmp_found = False
-    ret_found = False
-    for line in asm_lines:
-        if line.is_define:
-            if line.name == "TMP":
-                tmp_found = True
-            elif line.name == "RETURN_ADDRESS":
-                ret_found = True
-
-            if ret_found and tmp_found:
-                break
-
-    if not tmp_found:
-        print("No TMP variable!")
-    if not ret_found:
-        print("No RETURN_ADDRESS variable!")
-
-
-    calls = {}
-    for i, line in enumerate(asm_lines):
-        if line.is_instruction:
-            if line.opcode == "call" or line.opcode == "calli":
-                label = line.params[0]
-                if calls.get(label) is None:
-                    # [called, total, call_depth]
-                    calls[label] = [0, 0, 0]
-                else:
-                    calls[label][1] = calls[label][1] + 1
-
-    call_depth = 0
-    for i, line in enumerate(asm_lines):
-        if line.is_label:
-            if calls.get(line.value) is not None:
-                # inside called function
-                call_depth = call_depth + 1
-        elif line.is_instruction:
-            if line.opcode == "ret":
-                call_depth = call_depth - 1
-            elif line.opcode == "call" or line.opcode == "calli":
-                calls[line.params[0]][2] = call_depth
-
-
-    call_found = True
-    while call_found:
-        call_found = False
-        call_index = 0
-        label = ""
-        insert_1 = []
-        insert_2 = []
-        return_address = "RETURN_ADDRESS"
-
-        for i, line in enumerate(asm_lines):
-            if line.is_instruction:
-                # calli = Call with parameter in accumulator
-                # call = no parameter
-                # So we save parameter for calli in TMP var
-                # TODO: remove TMP var, save RETURN_ADDRESS before lit/ld to accumulator
-                if line.opcode == "call" or line.opcode == "calli":
-                    call_found = True
-                    call_index = i
-                    label = line.params[0]
-                    count = calls[label][0]
-                    depth = calls[label][2]
-                    if depth != 0:
-                        return_address = "RETURN_ADDRESS_" + str(depth)
-                    calls[label][0] = calls[label][0] + 1
-                    if line.opcode == "calli":
-                        insert_1.append(AsmLine("{0:4}st TMP".format(line.local_label), 0))
-                        insert_1.append(AsmLine("    lit #" + str(count), 0))
-                    else:
-                        insert_1.append(AsmLine("{0:4}lit #{1}".format(line.local_label, count), 0))
-
-                    insert_1.append(AsmLine("    st " + return_address, 0))
-                    if line.opcode == "calli":
-                        insert_1.append(AsmLine("    ld TMP", 0))
-
-                    insert_1.append(AsmLine("    jmp " + label, 0))
-                    insert_1.append(AsmLine(label + "_ret_" + str(count) + ":", 0))
-
-                    if count == 0:
-                        insert_2.append(AsmLine("    ld " + return_address, 0))
-                    else:
-                        insert_2.append(AsmLine("    cmpi #" + str(count), 0))
-
-                    insert_2.append(AsmLine("    jz " + label + "_ret_" + str(count), 0))
-                    break
-
-        if call_found:
-            search_for_ret = False
-            return_index = 0
-
-            for i, line in enumerate(asm_lines):
-                if search_for_ret:
-                    if line.is_instruction:
-                        if line.opcode == "ret":
-                            return_index = i
-                            if line.is_local_label:
-                                if calls[label][0] == 1:
-                                    insert_2[0] = AsmLine("{0:4}ld {1}".format(line.local_label, return_address), 0)
-
-                            break
-                elif line.is_label:
-                    if line.value == asm_lines[call_index].params[0]:
-                        search_for_ret = True
-
-            if return_index == 0:
-                print("No return index!!!")
-                break
-
-            if calls[label][0] > calls[label][1]:
-                asm_lines.remove(asm_lines[return_index])
-                asm_lines[return_index:return_index] = [AsmLine("    jmp halt ; invalid return address", 0)]
-
-
-            asm_lines[return_index:return_index] = insert_2
-
-            asm_lines.remove(asm_lines[call_index])
-            asm_lines[call_index:call_index] = insert_1
-
-            update_line_nums(asm_lines)
-
-
+def write_file(filename, asm_lines):
     with open(filename[:-4] + "u.asm", "w") as f:
         for line in asm_lines:
             f.write(line.line + "\n")
+
+def main():
+    if len(sys.argv) != 2:
+        print("Usage: preprocessor.py <asm file>")
+        return
+
+    filename = sys.argv[1]
+    with open(filename, "r") as f:
+        text_lines = f.readlines()
+
+    asm_lines = process_asm(text_lines)
+    write_file(filename, asm_lines)
 
 if __name__ == '__main__':
     main()
